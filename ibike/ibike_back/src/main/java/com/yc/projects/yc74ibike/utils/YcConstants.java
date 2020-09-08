83dd8f643ce4e22745c767a12cb4b6a06653767c
q1287938261 <1287938261@qq.com>
2020-09-08 17:33:55 +0800

    数据库迁移至mysql

--- ibike/ibike_back/src/main/java/com/yc/projects/yc74ibike/utils/YcConstants.java
+++ ibike/ibike_back/src/main/java/com/yc/projects/yc74ibike/utils/YcConstants.java
@@ -4,30 +4,30 @@ public class YcConstants {
 	/**
 	 * 短信 appid
 	 */
-	public static final String SMSAPPID = "1400387074";
+	public static final String SMSAPPID = "";
 	/**
 	 * 腾讯云账户密钥对: secretId
 	 */
-	public static final String SMSSECREID = "AKIDpTN1QeIoiOo3MPg6xa4PZEpfvxiCbPAD";
+	public static final String SMSSECREID = "";
 	/**
 	 * 腾讯云账户密钥对: secretKey
 	 */
-	public static final String SMSSECREKEY = "qc8NM9lCnEWeaFeohMmjRuCUW1LYiaCY";
+	public static final String SMSSECREKEY = "";
 	/**
 	 * SmsSdkAppid
 	 */
-	public static final String SMSSDKAPPID = "1400387074";
+	public static final String SMSSDKAPPID = "";
 	/**
 	 * sms sign : sms签名
 	 */
-	public static final String SMSSIGN = "可爱的小doom";
+	public static final String SMSSIGN = "";
 	/**
 	 * sms 短信模板id:
 	 */
-	public static final String SMSTEMPLATEID = "641454";
+	public static final String SMSTEMPLATEID = "";
 
-	public static final String SECRET = "6bb1356cd4071b9217eb52839abb176a";
+	public static final String SECRET = "";
 
-	public static final String APPID = "wx80ba4d16d14fb6a9";
+	public static final String APPID = "";
 
 }
